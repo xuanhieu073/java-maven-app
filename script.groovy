26ca8a91ead389df689d6900464c06484b9096cf
Hieu Nguyen Xuan <hieu.nguyenxuan@decision-science.com>
2024-06-04 15:14:04 +0700

    add docker-compose

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credential', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t xuanhieu073/demo-app:jma-2.0 ."
+       
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push xuanhieu073/demo-app:jma-2.0"
     }
@@ -14,9 +14,11 @@ def buildImage() {
 
 def deployApp() {
     echo "deploying the application..."
-    def dockerCmd = "docker run -p 8080:8080 -d xuanhieu073/demo-app:jma-2.0"
+    // def dockerCmd = "docker run -p 8080:8080 -d xuanhieu073/demo-app:jma-2.0"
+    def dockerComposeCmd = "docker-compose -f docker-compose.yaml up --detach"
     sshagent(['my-server-key']) {
-        sh "ssh -o StrictHostKeyChecking=no root@178.128.88.219 ${dockerCmd}"
+        sh "scp docker-compose.yaml root@178.128.88.219:/root"
+        sh "ssh -o StrictHostKeyChecking=no root@178.128.88.219 ${dockerComposeCmd}"
     }
 }
 
