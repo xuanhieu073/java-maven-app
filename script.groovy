e32867eecd50590c2fafb4fe43b0d16ca290c538
Nguyen Xuan Hieu <32063031+xuanhieu073@users.noreply.github.com>
2024-10-25 11:28:18 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credential', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-       
+        sh 'docker build -t xuanhieu073/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push xuanhieu073/demo-app:jma-2.0"
     }
